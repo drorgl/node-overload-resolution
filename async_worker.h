bc873a703db943c90c85dba2b27698d04eb3452c
Dror Gluska <drorgl@yahoo.com>
2016-12-14 22:18:39 +0200

    add to FunctionCallbackInfo.h prefetch a check for non-existing parameters, occurs when a function is called with more arguments than function parameters, also post processing set should happen only when called synchronously. extract types into overload_resolution_types.h modify overload_info constructor to avoid creating default parameters as Nan::Undefined() create an automatic async callback invocation when the last function argument is a callback function. add busy wait for tester.js async tests since the callbacks are "weak" and the application ends before callbacks have been verified.

---
+++ async_worker.h
@@ -0,0 +1,99 @@
+#ifndef _O_R_ASYNC_WORKER_H_
+#define _O_R_ASYNC_WORKER_H_
+
+#include <nan.h>
+#include <uv.h>
+#include "overload_resolution_types.h"
+
+namespace or {
+
+	class async_worker {
+	public:
+		uv_work_t request;
+
+		async_worker(PolyFunctionCallback &func, std::shared_ptr< or ::FunctionCallbackInfo<v8::Value>> params,std::shared_ptr< Nan::Callback> async_cb) :_func(func), _async_cb(async_cb)  {
+			_params = params;
+			request.data = this;
+			_params->prefetch();
+		}
+
+		void execute() {
+			try {
+				_func(*_params);
+			}
+			catch (std::exception ex) {
+				_error = ex.what();
+			}
+		}
+
+		void complete() {
+			Nan::HandleScope scope;
+
+			try {
+				_params->post_process(true);
+			}
+			catch (std::exception ex) {
+				_error = ex.what();
+			}
+
+
+			//TODO: !!!!!!!!!!! call ALL callbacks post process
+
+			//TODO: call the async callback function
+			if (_error == "") {
+				v8::Local<v8::Value> argv[] = {
+					Nan::Null(),
+					_params->Return()->Get()
+				};
+
+				_async_cb->Call(2, argv);
+			}
+			else {
+				v8::Local<v8::Value> argv[] = {
+					Nan::New(_error).ToLocalChecked(),
+					Nan::Null()
+				};
+
+				_async_cb->Call(2, argv);
+			}
+
+			/*if (_error != "") {
+				Nan::ThrowError(Nan::New(_error).ToLocalChecked());
+			}*/
+		}
+
+	protected:
+		PolyFunctionCallback &_func;
+		std::shared_ptr< or ::FunctionCallbackInfo<v8::Value>> _params;
+		std::string _error;
+		std::shared_ptr< Nan::Callback> _async_cb;
+	};
+
+	inline void async_execute(uv_work_t* req) {
+		async_worker *worker = static_cast<async_worker*>(req->data);
+		worker->execute();
+	}
+
+	inline void async_execute_complete(uv_work_t* req) {
+		async_worker* worker = static_cast<async_worker*>(req->data);
+		worker->complete();
+		delete worker;
+	}
+
+	void async_queue_worker(async_worker* worker) {
+		uv_queue_work(
+			uv_default_loop()
+			, &worker->request
+			, async_execute
+			, reinterpret_cast<uv_after_work_cb>(async_execute_complete)
+			);
+	}
+
+	void queue_async_polyfunction(PolyFunctionCallback &func, std::shared_ptr< or ::FunctionCallbackInfo<v8::Value>> params, std::shared_ptr<Nan::Callback> async_cb) {
+		auto async_instance = new async_worker(func, params, async_cb);
+		async_queue_worker(async_instance);
+	}
+	
+}
+
+#endif
